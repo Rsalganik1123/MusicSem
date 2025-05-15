3e6514494f465d6d26e354a526320ea987a44242
Rebecca Salganik <rsalgani@node2x64a.cs.rochester.edu>
2025-05-15 19:25:48 -0400

    toolkit with args

--- evaluations/t2m/t2m_canon.py
+++ evaluations/t2m/t2m_canon.py
@@ -6,7 +6,9 @@ import shutil
 import argparse  
 import ipdb 
 
-
+from eval_clap import run_clap_eval 
+from eval_ldm import run_ldm_eval 
+from eval_vendi import run_vendi_eval 
 
 prompt_dict = {
     'music_caps': '/data2/rsalgani/music-caps/musiccaps-public.csv', 
@@ -14,6 +16,7 @@ prompt_dict = {
     'test_set_May3': '/data2/rsalgani/hallucination/test.csv', 
     'test_set_May6': '/data2/rsalgani/reddit/test_sets/test_set_May6/package/test_May6_final.csv'
 }
+
 audio_dict = {
     'music_caps': ['/data2/rsalgani/music-caps/wav/', 'wav'], 
     'song_describer': ['/data2/rsalgani/song_describer/audio/', 'mp3'],
@@ -22,10 +25,17 @@ audio_dict = {
 
 def parse_args(): 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--mode', choices=['eval', 'gather'])
-    parser.add_argument('--gen_model', choices=['musicgen', 'stable_audio', 'musiclm', 'mustango', 'audioldm2'])
+    parser.add_argument('--gen_model', choices=['musicgen', 'stable_audio', 'musiclm', 'mustango', 'audioldm2', 'mureka'])
     parser.add_argument('--mc_path', default='/data2/rsalgani/music-caps/wav/')
-    parser.add_argument('--audio_save_dir', default='/data2/rsalgani/saved_embeddings/')
-    parser.add_argument('--save_dir', default='/data2/rsalgani/eval_outputs/fad/')
+    parser.add_argument('--gen_dir', default='/data2/rsalgani/saved_embeddings/')
+    parser.add_argument('--dataset', choices=['music_caps', 'song_describer', 'test_set_May6'])
+    parser.add_argument('--save_dir', default='/data2/rsalgani/eval_outputs/')
+    parser.add_argument('--metric', choices=['CLAP', 'KLD', 'VS'])
     return parser.parse_args()
 
+    
+if __name__ == "__main__": 
+    args = parse_args() 
+    data_path = audio_dict[args.dataset]
+    if args.metric == 'CLAP': 
+        run_clap_eval(args, data_path)
